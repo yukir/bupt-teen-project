d8bb111226c7e2addcaece9fb54687182a5e173a
Vitech Liu <liuzirui1122@163.com>
2018-03-16 02:20:58 +0800

    活动创建 validator,policy

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Activity;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ActivityController extends Controller
 {
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -24,7 +32,7 @@ class ActivityController extends Controller
      */
     public function create()
     {
-        //
+        $this->middleware('auth');
     }
 
     /**
@@ -34,8 +42,28 @@ class ActivityController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
+    
+        $this->middleware('auth');
+    
+        //活动表单后端验证
+        $validatedData = $request-validate([
+            'title' => 'required|string|max:191|unique:activities' ,
+            'type' => [
+                'required',
+                Rule::in(['sxyl','yxtx','mzy','zttr','tgpx', 'xywh'])
+            ],
+            'content' => 'required|max:65535',
+            'start_at' => 'nullable|date|after:yesterday',
+            'check_required' => 'required|boolean',
+            'community_day_id' => 'nullable|numeric'
+        ]);
+
+        //权限后端验证
+        if (auth()->user()->can('createWithType',$request->input('type'))) {
+            $a = new Activity($request->only['title','content','type','start_at','check_required','community_day_id']);
+            auth()->user()->activities()->save($a);
+        } else abort(403);
+        
     }
 
     /**
